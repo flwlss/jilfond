4a345c7749c08cfb8086dd3fae23fc85d322cba7
Dmitry <dimaandreev1337@gmail.com>
2023-08-21 17:00:45 +0700

    added modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <span className="modal__spinner" />
+    </div>
+  );
+};
+
+export default Modal;
